f2f38d7c374c5e069cad3c431453311a6ba3a5cb
kjones <k_jones_17@yahoo.com>
2020-04-17 17:07:29 -0500

    Basic hello world with pymongo basics commented

---
+++ HelloWorld.py
@@ -0,0 +1,13 @@
+#import pymongo
+
+def main():
+	##begin mongoDB connection
+	#client = pymongo.MongoClient(
+	#	"mongodb+srv://root:chicago1%21@blackjackanalytics-idjco.mongodb.net/Results?retryWrites=true&w=majority&authSource=admin")
+	#db = client.Results
+	#coll = db.TestColResults
+
+    #Hello World
+    print("hello world")
+
+main()
